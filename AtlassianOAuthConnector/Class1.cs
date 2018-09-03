a8af58bb198db1e7a0e202c17349386bc3586641
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-09-03 12:56:34 +0200

    Add project files.

---
+++ AtlassianOAuthConnector/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtlassianOAuthConnector
+{
+    public class Class1
+    {
+    }
+}
