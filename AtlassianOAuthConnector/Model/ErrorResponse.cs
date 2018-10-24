756134d25f77615171ede1de74b90b5a4a2d0032
LUBOMYL <LUBOMYL@NTB-LUBOMYL>
2018-10-24 10:53:15 +0200

    WIP: implementing http errors handling

---
+++ AtlassianOAuthConnector/Model/ErrorResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtlassianConnector.Model
+{
+    public class ErrorResponse
+    {
+
+        public Dictionary<string, string> Errors { get; set; }
+
+    }
+}
---
+++ AtlassianOAuthConnector/Model/Exceptions/MissingParameterException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtlassianConnector.Model.Exceptions
+{
+    public class MissingParameterException : Exception
+    {
+
+        public ErrorResponse ErrorResponse { get; set; }
+
+        public MissingParameterException(ErrorResponse er) : base()
+        {
+            this.ErrorResponse = er;
+        }
+
+    }
+}
